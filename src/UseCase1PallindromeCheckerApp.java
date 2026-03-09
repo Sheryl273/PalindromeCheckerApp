21b7f6a761ab3585e2f09ed1ffe30ec7fe7e7f4a
Sheryl Chauhan <sherylchauhan@Sheryls-MacBook-Air.local>
2026-03-09 12:27:06 +0530

    Initial commit - Palindrome Checker UC1

---
+++ src/UseCase1PallindromeCheckerApp.java
@@ -0,0 +1,11 @@
+// @author Sheryl Chauhan
+// @version 1.0
+// Use case 1 : Welcome Page
+
+
+public class UseCase1PallindromeCheckerApp {
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Pallindrome Checker Management System\nVersion: 1.0\nSystem Initialized Succesfully");
+    }
+}
